9b92a75e93ee93bddaa1f6f4f6346b31731f1b81
Francisco <fcomancillaj@gmail.com>
2024-01-31 14:19:28 -0400

    terminando logica de colores.

--- src/assets/js/dataInjection.js
+++ src/assets/js/dataInjection.js
@@ -1,6 +1,14 @@
+const SPJG = [1.3, 1.3, 1.0, 1.0, 0.8, 0.6, 0.5];
+const SPHF = [5.0, 5.0, 6.0, 7.0, 8.0, 8.0, 10.0];
+
+let diferenciasPorcentuales = { jg: [], hf: [] };
+
+function truncarADosDecimales(numero) {
+    return Math.trunc(numero * 100) / 100;
+}
+
 function solicitarDatos(celda, columnas, elementoJG, elementoHF) {
     const datos = { celda: 'celda_' + celda, columnas: columnas };
-
     fetch(apiConfig.datosChart, {
         method: 'POST',
         headers: {
@@ -15,12 +23,36 @@ function solicitarDatos(celda, columnas, elementoJG, elementoHF) {
             return response.json();
         })
         .then(data => {
-            if (elementoJG) elementoJG.innerHTML = `JG <br>${data.jg}`;
-            if (elementoHF) elementoHF.innerHTML = `HF <br>${data.hf}`;
+            if (elementoJG) elementoJG.innerHTML = `JG: <br>${truncarADosDecimales(data.jg)}`;
+            if (elementoHF) elementoHF.innerHTML = `HF: <br>${truncarADosDecimales(data.hf)}`;
+            evaluarYActualizarClases(data, celda - 1);
         })
         .catch(error => {
             console.error('Error al hacer la solicitud:', error);
         });
+}
+
+function evaluarYActualizarClases(data, indiceCelda) {
+    const elementoJG = document.getElementById(`celda${indiceCelda + 1}-jg`);
+    const elementoHF = document.getElementById(`celda${indiceCelda + 1}-hf`);
+
+    const diferenciaJG = Math.abs(data.jg - SPJG[indiceCelda]) / SPJG[indiceCelda];
+    const diferenciaHF = Math.abs(data.hf - SPHF[indiceCelda]) / SPHF[indiceCelda];
+
+    diferenciasPorcentuales.jg[indiceCelda] = diferenciaJG;
+    diferenciasPorcentuales.hf[indiceCelda] = diferenciaHF;
+
+    actualizarClase(elementoJG, diferenciaJG);
+    actualizarClase(elementoHF, diferenciaHF);
+}
+
+function actualizarClase(elemento, diferencia) {
+    elemento.classList.remove('btn-danger', 'btn-warning');
+    if (diferencia > 0.2) {
+        elemento.classList.add('btn-danger');
+    } else if (diferencia > 0.1) {
+        elemento.classList.add('btn-warning');
+    }
 }
 
 function actualizarDatos() {
@@ -32,9 +64,8 @@ function actualizarDatos() {
         }
     }
 }
-
 function solicitarRO(nombreCelda, elementoRO) {
-    const datos = { celda: nombreCelda, columnas: ['ro'] }; // Solo estamos interesados en la columna 'ro'
+    const datos = { celda: nombreCelda, columnas: ['ro'] };
 
     fetch(apiConfig.datosChart, {
         method: 'POST',
@@ -50,18 +81,17 @@ function solicitarRO(nombreCelda, elementoRO) {
             return response.json();
         })
         .then(data => {
-            elementoRO.innerHTML = `RO <br>${data.ro}`; // Asegúrate de que 'data.ro' es la propiedad correcta
+            elementoRO.innerHTML = `RO <br>${truncarADosDecimales(data.ro)}`;
         })
         .catch(error => {
             console.error('Error al hacer la solicitud:', error);
         });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    let elementoRO = document.getElementById('valor-ro');
+document.addEventListener('DOMContentLoaded', function() {    let elementoRO = document.getElementById('valor-ro');
     if (elementoRO) {
-        solicitarRO('celda_1', elementoRO); // Asegúrate de que 'nombreDeLaCeldaParaRO' sea el nombre correcto
+        solicitarRO('celda_1', elementoRO);
     }
-    actualizarDatos(); // Actualizar los datos inmediatamente al cargar la página
+
+    actualizarDatos();
     setInterval(actualizarDatos, 600000); // 600000 milisegundos = 10 minutos
 });
